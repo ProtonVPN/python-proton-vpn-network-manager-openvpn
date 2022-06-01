374a932c7077a898c0a4de7661b731246b65ac15
Pep Llaneras <josep.llaneras@proton.ch>
2022-06-02 00:55:51 +0200

    Make sure the network manager connection has been created before activating it

--- proton/vpn/backend/linux/networkmanager/protocol/openvpn/openvpn.py
+++ proton/vpn/backend/linux/networkmanager/protocol/openvpn/openvpn.py
@@ -102,7 +102,8 @@ class OpenVPN(LinuxNetworkManager):
         vpnconfig.use_certificate = self._use_certificate
 
         self._configure_connection(vpnconfig)
-        self.nm_client._add_connection_async(self.connection)
+        future = self.nm_client._add_connection_async(self.connection)
+        future.result()  # FIXME: we should probably return the future instead
 
 
 class OpenVPNTCP(OpenVPN):
